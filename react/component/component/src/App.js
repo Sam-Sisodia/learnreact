f1decb3416ba2871dc2207bf789687b47a90db38
nisha <nisha.sadyan@shubpy.com>
2023-06-22 23:58:13 +0530

    basic componet

---
+++ react/component/component/src/App.js
@@ -0,0 +1,13 @@
+import React , {Component}  from  "react";
+
+class App extends Component{
+    
+    render() {
+        return <h1>This is componet Demo </h1>
+        
+    };
+
+
+}
+
+export default  App;
